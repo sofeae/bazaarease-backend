cc88a70e637be809d4eb2a70dac0944ec6cbd93b
Adly <adly.syaukat@gmail.com>
2023-12-22 23:22:36 +0800

    Fix QR

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,40 +1,47 @@
-const User = require('../models/userModel')
-const jwt = require('jsonwebtoken')
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-}
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login a user
 const loginUser = async (req, res) => {
-  const {email, password, businessName} = req.body
+  const { email, password, businessName } = req.body;
 
   try {
-    const user = await User.login(email, password, businessName)
+    const user = await User.login(email, password, businessName);
 
     // create a token
-    const token = createToken(user._id)
+    const token = createToken(user._id);
 
-    res.status(200).json({email, token})
+    const obj = {
+      id: user._id,
+      email,
+      token,
+      businessName,
+    };
+
+    res.status(200).json(obj);
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
 // signup a user
 const signupUser = async (req, res) => {
-  const {email, password, businessName} = req.body
+  const { email, password, businessName } = req.body;
 
   try {
-    const user = await User.signup(email, password, businessName)
+    const user = await User.signup(email, password, businessName);
 
     // create a token
-    const token = createToken(user._id)
+    const token = createToken(user._id);
 
-    res.status(200).json({email, token})
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
-module.exports = { signupUser, loginUser } 
+module.exports = { signupUser, loginUser };
