ad3346361b175518098cb1c267e8f4836fa5c134
sofeae <sofeaezzaty@gmail.com>
2024-01-02 23:45:39 +0800

    update order auth

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -11,21 +11,21 @@ const requireAuth = require("../middleware/requireAuth");
 const router = express.Router();
 
 // require auth for all menu routes
-router.use(requireAuth)
+// router.use(requireAuth)
 
 // GET all orders
-router.get("/", getOrders);
+router.get("/", requireAuth, getOrders);
 
 //GET a single order
-router.get("/:id", getOrder);
+router.get("/:id", requireAuth, getOrder);
 
 //POST creata a order
 router.post("/", createOrder);
 
 // DELETE a menu
-router.delete("/:id", deleteOrder);
+router.delete("/:id", requireAuth, deleteOrder);
 
 // UPDATE a order
-router.patch("/:id", updateOrder);
+router.patch("/:id", requireAuth, updateOrder);
 
 module.exports = router;
